a496fe0dd892ddc76491cace64766a4b3de71e4d
Sunita Kachornvitaya <sunita.kachornvitaya@g.swu.ac.th>
2019-04-29 14:10:22 +0700

    edit

--- main.py
+++ main.py
@@ -20,50 +20,50 @@ datalocal = DataStore()
 databook = pd.read_json('booksdata.json')
 ratings = pd.read_csv('ratings.csv')
 
-# tf =  TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
-# tfidf_matrix = tf.fit_transform(databook['feature'])
+tf =  TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
+tfidf_matrix = tf.fit_transform(databook['feature'])
 
-# cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-# titles = databook[['title','authors','small_image_url','average_rating']]
+titles = databook[['title','authors','small_image_url','average_rating']]
 
-# reader = Reader()
-# data = Dataset.load_from_df(ratings[['user_id', 'book_id', 'rating']], reader)
-# svd = SVD()
-# trainset = data.build_full_trainset()
-# svd.train(trainset)
+reader = Reader()
+data = Dataset.load_from_df(ratings[['user_id', 'book_id', 'rating']], reader)
+svd = SVD()
+trainset = data.build_full_trainset()
+svd.train(trainset)
 
-# def find_feature(title):
-#   feature =  databook[(databook['title'] == title)].index[0]
-#   return feature
+def find_feature(title):
+  feature =  databook[(databook['title'] == title)].index[0]
+  return feature
 
-# def get_recommendations(title):
-#     idx = find_feature(title)
-#     sim_scores = list(enumerate(cosine_sim[idx]))
-#     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-#     sim_scores = sim_scores[1:31]
-#     book_indices = [i[0] for i in sim_scores]
-#     return titles.iloc[book_indices]
+def get_recommendations(title):
+    idx = find_feature(title)
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:31]
+    book_indices = [i[0] for i in sim_scores]
+    return titles.iloc[book_indices]
 
-# def hybrid(userId):
-#   userbooks = ratings[(ratings['user_id'] == userId)]
-#   userbooks = userbooks.merge(databook[['title', 'book_id']], on='book_id')
-#   titlebook = list(userbooks['title'])
+def hybrid(userId):
+  userbooks = ratings[(ratings['user_id'] == userId)]
+  userbooks = userbooks.merge(databook[['title', 'book_id']], on='book_id')
+  titlebook = list(userbooks['title'])
   
-#   bookrecs = pd.DataFrame()
+  bookrecs = pd.DataFrame()
 
-#   for x in titlebook:
-#       idx = find_feature(x)
-#       sim_scores = list(enumerate(cosine_sim[idx]))
-#       sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-#       sim_scores = sim_scores[1:31]
-#       book_indices = [i[0] for i in sim_scores]
-#       bookrec = databook.iloc[book_indices][['book_id','title','authors','small_image_url','average_rating']]
-#       bookrecs = pd.concat([pd.DataFrame(bookrec), bookrecs], ignore_index=True)
-#       bookrecs['est'] = bookrecs['book_id'].apply(lambda x: svd.predict(userId, x).est)
-#       bookrecs = bookrecs.sort_values('est', ascending=False)
+  for x in titlebook:
+      idx = find_feature(x)
+      sim_scores = list(enumerate(cosine_sim[idx]))
+      sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+      sim_scores = sim_scores[1:31]
+      book_indices = [i[0] for i in sim_scores]
+      bookrec = databook.iloc[book_indices][['book_id','title','authors','small_image_url','average_rating']]
+      bookrecs = pd.concat([pd.DataFrame(bookrec), bookrecs], ignore_index=True)
+      bookrecs['est'] = bookrecs['book_id'].apply(lambda x: svd.predict(userId, x).est)
+      bookrecs = bookrecs.sort_values('est', ascending=False)
       
-#   return bookrecs.head(10)
+  return bookrecs.head(10)
 
 @app.route('/')
 def index():
@@ -89,14 +89,15 @@ def live_data():
         while True:    
             if datalocal.facerec != None:
                 datalocal.facerec = None
-                # book_list = hybrid(int(2))
-                book_list = databook.head(5)
+                book_list = hybrid(int(18052539))
+                # book_list = databook.head(5)
                 bookjson = []
                 for index, row in book_list.iterrows():
-                    data ={"title": row['title'],
-                           "authors": row['authors'],
-                           "img": row['small_image_url'],
-                           "rating": math.ceil(row['average_rating'] * 2) / 2 ,
+                    data ={
+                        "title": row['title'],
+                        "authors": row['authors'],
+                        "img": row['small_image_urls'],
+                        "rating": math.ceil(row['average_rating'] * 2) / 2 
                     }
                     bookjson.append(data)
 
